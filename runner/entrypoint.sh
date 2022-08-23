2c2be9a9a29dd1d1f9a4c4b773882e5ef93017a3
Xose Pérez <xose.perez@gmail.com>
2022-08-24 01:22:45 +0200

    Add option to define TTS port, clean up hostst

--- runner/entrypoint.sh
+++ runner/entrypoint.sh
@@ -7,6 +7,7 @@ replace_placeholders() {
     sed -i -e "s/{{noreply_email}}/${TTS_NOREPLY_EMAIL}/g" $FILE
     sed -i -e "s/{{console_secret}}/${TTS_CONSOLE_SECRET}/g" $FILE
     sed -i -e "s/{{domain}}/${TTS_DOMAIN}/g" $FILE
+    sed -i -e "s/{{port}}/${TTS_PORT}/g" $FILE
     sed -i -e "s/{{mail_provider}}/${MAIL_PROVIDER}/g" $FILE
     sed -i -e "s/{{sendgrid_key}}/${TTS_SENDGRID_KEY}/g" $FILE
     sed -i -e "s/{{smtp_host}}/${TTS_SMTP_HOST}/g" $FILE
@@ -59,6 +60,7 @@ STACK_CONFIG_FILE=${HOME_FOLDER}/ttn-lw-stack-docker.yml
 CLI_CONFIG_FILE=${HOME_FOLDER}/.ttn-lw-cli.yml
 
 # Get configuration variables
+TTS_PORT=${TTS_PORT:-443}
 TTS_SERVER_NAME=${TTS_SERVER_NAME:-The Things Stack}
 TTS_ADMIN_EMAIL=${TTS_ADMIN_EMAIL:-admin@thethings.example.com}
 TTS_NOREPLY_EMAIL=${TTS_NOREPLY_EMAIL:-noreply@thethings.example.com}
@@ -102,23 +104,26 @@ TTS_SUBJECT_COUNTRY=${TTS_SUBJECT_COUNTRY:-ES}
 TTS_SUBJECT_STATE=${TTS_SUBJECT_STATE:-Catalunya}
 TTS_SUBJECT_LOCATION=${TTS_SUBJECT_LOCATION:-Barcelona}
 TTS_SUBJECT_ORGANIZATION=${TTS_SUBJECT_ORGANIZATION:-TTN Catalunya}
-EXPECTED_SIGNATURE="$TTS_SUBJECT_COUNTRY $TTS_SUBJECT_STATE $TTS_SUBJECT_LOCATION $TTS_SUBJECT_ORGANIZATION $TTS_DOMAIN"
+DOMAINS=$(echo "$TTS_DOMAIN,localhost,$IP_LAN" | sed 's/,$//')
+EXPECTED_SIGNATURE="$TTS_SUBJECT_COUNTRY $TTS_SUBJECT_STATE $TTS_SUBJECT_LOCATION $TTS_SUBJECT_ORGANIZATION $DOMAINS"
 CURRENT_SIGNATURE=$(cat ${DATA_FOLDER}/certificates_signature 2> /dev/null)
 
 if [ "$CURRENT_SIGNATURE" != "$EXPECTED_SIGNATURE" ]; then
 
     cd /tmp
     
-    echo '{"CN":"'$TTS_SUBJECT_ORGANIZATION CA'","key":{"algo":"rsa","size":2048},"names":[{"C":"'$TTS_SUBJECT_COUNTRY'","ST":"'$TTS_SUBJECT_STATE'","L":"'$TTS_SUBJECT_LOCATION'","O":"'$TTS_SUBJECT_ORGANIZATION'"}]}' > ca.json
+    echo '{"CN":"'$TTS_SUBJECT_ORGANIZATION' CA","names":[{"C":"'$TTS_SUBJECT_COUNTRY'","ST":"'$TTS_SUBJECT_STATE'","L":"'$TTS_SUBJECT_LOCATION'","O":"'$TTS_SUBJECT_ORGANIZATION'"}],"key":{"algo":"rsa","size":2048}}' > ca.json
     cfssl genkey -initca ca.json | cfssljson -bare ca
 
-    echo '{"CN":"'$TTS_DOMAIN'","hosts":["'$TTS_DOMAIN'","localhost","'$(echo $IP_LAN | sed 's/,/\",\"/')'"],"key":{"algo":"rsa","size":2048},"names":[{"C":"'$TTS_SUBJECT_COUNTRY'","ST":"'$TTS_SUBJECT_STATE'","L":"'$TTS_SUBJECT_LOCATION'","O":"'$TTS_SUBJECT_ORGANIZATION'"}]}' > cert.json
-    cfssl gencert -hostname "$TTS_DOMAIN,localhost,$IP_LAN" -ca ca.pem -ca-key ca-key.pem cert.json | cfssljson -bare cert
+    echo '{"CN":"'$TTS_DOMAIN'","hosts":["'$(echo $DOMAINS | sed 's/,/\",\"/g')'"],"names":[{"C":"'$TTS_SUBJECT_COUNTRY'","ST":"'$TTS_SUBJECT_STATE'","L":"'$TTS_SUBJECT_LOCATION'","O":"'$TTS_SUBJECT_ORGANIZATION'"}],"key":{"algo":"rsa","size":2048}}' > cert.json
+    cfssl gencert -hostname "$DOMAINS" -ca ca.pem -ca-key ca-key.pem cert.json | cfssljson -bare cert
 
     cp ca.pem ${DATA_FOLDER}/ca.pem
     cp ca-key.pem ${DATA_FOLDER}/ca-key.pem
     cp cert.pem ${DATA_FOLDER}/cert.pem
     cp cert-key.pem ${DATA_FOLDER}/key.pem
+
+    chown 886:886 ${DATA_FOLDER}/cert.pem ${DATA_FOLDER}/key.pem
 
     echo $EXPECTED_SIGNATURE > ${DATA_FOLDER}/certificates_signature
 
